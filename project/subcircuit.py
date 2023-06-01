c0c9ea7a7a3f4ec561ba8389e1e76d39db841362
Yucheng Wang <kris_7z@hotmail.com>
2023-05-31 21:15:24 -0700

    single-path circuit

--- project/subcircuit.py
+++ project/subcircuit.py
@@ -18,21 +18,22 @@ def hash_node_name(node_name, direction):
     # regex to remove all non-alphanumeric characters
     node_name = re.sub(r"\W+", "", node_name)
     result = node_name + direction
+    return result
     # special case for input
-    if result == "00in":
-        return "input"
-    else:
-        return result
+    # if result == "00in":
+    #     return "input"
+    # else:
+    #     return result
 
 # hash the connections of an edge
 def hash_edge_name(node1, node2):
-    return hash_node_name(node1, "out"), hash_node_name(node2, "in")
+    return hash_node_name(node1, "in"), hash_node_name(node2, "in")
 
 ##################### GENERATE CIRCUIT GRAPH #####################
 # use networkx to generate a graph of the circuit
-circuit_graph = nx.grid_graph(dim=[2, 2])
+circuit_graph = nx.grid_graph(dim=[2, 6])
 
-mst = nx.minimum_spanning_tree(circuit_graph)
+mst = nx.minimum_spanning_tree(circuit_graph, algorithm="prim")
 print(mst.nodes)
 
 # use node names to set the positions
@@ -53,7 +54,8 @@ labels = nx.get_node_attributes(mst, 'node_name')
 print("node labels", len(labels))
 
 # draw with node names and edge names
-# nx.draw_networkx(mst, pos, with_labels=True, labels=labels, font_weight="bold")
+nx.draw_networkx(mst, pos, with_labels=True, labels=labels, font_weight="bold")
+plt.show()
 
 # draw edge labels
 edge_labels = nx.get_edge_attributes(mst, 'edge_name')
@@ -68,6 +70,9 @@ for edge in mst.edges:
 libraries_path = find_libraries()
 spice_library = SpiceLibrary(libraries_path)
 
+period = 50@u_ns
+pulse_width = period/2
+
 # Voltage Pulse Configuration
 startVoltage = 0 @ u_V
 endVoltage = 1 @ u_V
@@ -76,7 +81,7 @@ riseTime = 1 @ u_us
 
 circuit = Circuit("test")
 circuit.PulseVoltageSource(
-    "pulse", "input", circuit.gnd, startVoltage, endVoltage, startTime, riseTime
+    "pulse", "51in", circuit.gnd, startVoltage, endVoltage, startTime, riseTime
 )
 
 componenet_idx = 0
@@ -85,11 +90,10 @@ for node in mst.nodes:
     # get the node name
     node_name = mst.nodes[node]["node_name"]
     node_name_in = hash_node_name(node_name, "in")
-    node_name_out = hash_node_name(node_name, "out")
+    # node_name_out = hash_node_name(node_name, "out")
     # each node is connected by its own in and out
-    circuit.C(componenet_idx, node_name_in, node_name_out, mst.nodes[node]["capacitance"] @ u_F)
+    circuit.C(componenet_idx, node_name_in, circuit.gnd, mst.nodes[node]["capacitance"] @ u_F)
     componenet_idx += 1
-    break
 
 for edge in mst.edges:
     # get the edge name
@@ -98,13 +102,11 @@ for edge in mst.edges:
     circuit.R(componenet_idx, edge_name_first, edge_name_second, mst.edges[edge]["resistance"] @ u_kΩ)
     componenet_idx += 1
 
-
-# R1 = circuit.R(1, "2", "input", 4 @ u_kΩ)
-# R2 = circuit.R(2,'p2','p6',1@u_kΩ)
-# C2 = circuit.C(2, "input", "6", 1e-9 @ u_F)
-# R3 = circuit.R(4, "5", "1", 1 @ u_kΩ)
-# R4 = circuit.R(3, "6", "5", 1 @ u_kΩ)
-# R5 = circuit.R(5, "0", "6", 1e-9 @ u_Ω)
+# C1 = circuit.C(1, "input", circuit.gnd, 1e-9 @ u_F)
+# R1 = circuit.R(2, "input", "c2in", 1 @ u_kΩ)
+# C2 = circuit.C(3, "c2in", circuit.gnd, 1e-9 @ u_F)
+# R2 = circuit.R(4, "c2in", "c3in", 1 @ u_kΩ)
+# C3 = circuit.C(5, "c3in", circuit.gnd, 1e-9 @ u_F)
 
 
 print(str(circuit))
